3fec4ba8f5d1becc7b6d7adcd684a04113726b9b
Avner <76203167+InsanelyAvner@users.noreply.github.com>
2022-10-18 11:38:53 +0700

    Update client.py

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 import socket
 from tkinter import *
 import tkinter as tk
-from  threading import Thread
+from threading import Thread
 import random
 from PIL import ImageTk, Image
 import platform
@@ -11,7 +11,7 @@ screen_width = None
 screen_height = None
 
 SERVER = None
-PORT  = 8000
+PORT = 8000
 IP_ADDRESS = '127.0.0.1'
 playerName = None
 
@@ -22,17 +22,36 @@ nameEntry = None
 nameWindow = None
 gameWindow = None
 
-ticketGrid  = []
+ticketGrid = []
 currentNumberList = []
 flashNumberList = []
 flashNumberLabel = None
+
+
+def recivedMsg():
+    global SERVER
+    global displayedNumberList
+    global flashNumberLabel
+    global canvas2
+    global gameOver
+    numbers = [str(i) for i in range(1, 91)]
+    while True:
+        chunk = SERVER. recv(2048).decode()
+        if chunk in numbers and flashNumberLabel and not gameOver:
+            flashNumberList.append(int(chunk))
+            canvas2.itemconfigure(flashNumberLabel, text=chunk, font=('Chalkboard SE', 60))
+
+        elif 'wins the game.' in chunk:
+            gameOver = True
+            canvas2.itemconfigure(flashNumberLabel, text=chunk, font=('Chalkboard SE', 40))
 
 
 def createTicket():
     global gameWindow
     global ticketGrid
     # Ticket Frame
-    mianLable = Label(gameWindow, width=65, height=16,relief='ridge', borderwidth=5, bg='white')
+    mianLable = Label(gameWindow, width=65, height=16,
+                      relief='ridge', borderwidth=5, bg='white')
     mianLable.place(x=95, y=119)
 
     xPos = 105
@@ -40,22 +59,22 @@ def createTicket():
     for row in range(0, 3):
         rowList = []
         for col in range(0, 9):
-            if(platform.system() == 'Darwin'):
+            if (platform.system() == 'Darwin'):
                 # For Mac users
                 boxButton = Button(gameWindow,
-                font = ("Chalkboard SE",18),
-                borderwidth=3,
-                pady=23,
-                padx=-22,
-                bg="#fff176", # Initial Yellow color
-                highlightbackground='#fff176',
-                activebackground='#c5e1a5') # onPress Green Color
-
+                                   font=("Chalkboard SE", 18),
+                                   borderwidth=3,
+                                   pady=23,
+                                   padx=-22,
+                                   bg="#fff176",  # Initial Yellow color
+                                   highlightbackground='#fff176',
+                                   activebackground='#c5e1a5')  # onPress Green Color
 
                 boxButton.place(x=xPos, y=yPos)
             else:
                 # For windows users
-                boxButton = tk.Button(gameWindow, font=("Chalkboard SE",30), width=3, height=2,borderwidth=5, bg="#fff176")
+                boxButton = tk.Button(gameWindow, font=(
+                    "Chalkboard SE", 30), width=3, height=2, borderwidth=5, bg="#fff176")
                 boxButton.place(x=xPos, y=yPos)
 
             rowList.append(boxButton)
@@ -63,23 +82,23 @@ def createTicket():
         # Creating nested array
         ticketGrid.append(rowList)
         xPos = 105
-        yPos +=82
+        yPos += 82
 
 
 def placeNumbers():
     global ticketGrid
     global currentNumberList
 
-    for row in range(0,3):
+    for row in range(0, 3):
         randomColList = []
         counter = 0
         # getting random 5 cols
-        while counter<=4:
-            randomCol = random.randint(0,8)
-            if(randomCol not in randomColList):
+        while counter <= 4:
+            randomCol = random.randint(0, 8)
+            if (randomCol not in randomColList):
                 randomColList.append(randomCol)
-                counter+=1
-    
+                counter += 1
+
     numberContainer = {
         "0": [i for i in range(1, 10)],
         "1": [i for i in range(10, 20)],
@@ -101,11 +120,10 @@ def placeNumbers():
         if randomNumber not in currentNumberList:
             numberBox = ticketGrid[row][colNum]
             numberBox.configure(text=randomNumber, fg="black")
-            
+
             currentNumberList.append(randomNumber)
 
             counter += 1
-
 
 
 def gameWindow():
@@ -118,7 +136,6 @@ def gameWindow():
     global resetButton
     global flashNumberLabel
 
-
     gameWindow = Tk()
     gameWindow.title("Tambola Family Fun")
     gameWindow.geometry('800x600')
@@ -126,27 +143,27 @@ def gameWindow():
     screen_width = gameWindow.winfo_screenwidth()
     screen_height = gameWindow.winfo_screenheight()
 
-    bg = ImageTk.PhotoImage(file = "./assets/background.png")
+    bg = ImageTk.PhotoImage(file="./assets/background.png")
 
-    canvas2 = Canvas( gameWindow, width = 500,height = 500)
-    canvas2.pack(fill = "both", expand = True)
+    canvas2 = Canvas(gameWindow, width=500, height=500)
+    canvas2.pack(fill="both", expand=True)
 
     # Display image
-    canvas2.create_image( 0, 0, image = bg, anchor = "nw")
+    canvas2.create_image(0, 0, image=bg, anchor="nw")
 
     # Add Text
-    canvas2.create_text( screen_width/4.5,50, text = "Tambola Family Fun", font=("Chalkboard SE",50), fill="#3e2723")
+    canvas2.create_text(screen_width/4.5, 50, text="Tambola Family Fun",
+                        font=("Chalkboard SE", 50), fill="#3e2723")
 
     createTicket()
     placeNumbers()
 
-
     # Flash Number Label
-    flashNumberLabel = canvas2.create_text(400,screen_height/2.3, text = "Waiting for other players to join...", font=("Chalkboard SE",30), fill="#3e2723")
+    flashNumberLabel = canvas2.create_text(
+        400, screen_height/2.3, text="Waiting for other players to join...", font=("Chalkboard SE", 30), fill="#3e2723")
 
     gameWindow.resizable(True, True)
     gameWindow.mainloop()
-
 
 
 def saveName():
@@ -164,40 +181,38 @@ def saveName():
     gameWindow()
 
 
-
 def askPlayerName():
     global playerName
     global nameEntry
     global nameWindow
     global canvas1
 
-    nameWindow  = Tk()
+    nameWindow = Tk()
     nameWindow.title("Tambola Family Fun")
     nameWindow.geometry('800x600')
-
 
     screen_width = nameWindow.winfo_screenwidth()
     screen_height = nameWindow.winfo_screenheight()
 
-    bg = ImageTk.PhotoImage(file = "./assets/background.png")
+    bg = ImageTk.PhotoImage(file="./assets/background.png")
 
-    canvas1 = Canvas( nameWindow, width = 500,height = 500)
-    canvas1.pack(fill = "both", expand = True)
+    canvas1 = Canvas(nameWindow, width=500, height=500)
+    canvas1.pack(fill="both", expand=True)
     # Display image
-    canvas1.create_image( 0, 0, image = bg, anchor = "nw")
-    canvas1.create_text( screen_width/4.5,screen_height/8, text = "Enter Name", font=("Chalkboard SE",60), fill="#3e2723")
+    canvas1.create_image(0, 0, image=bg, anchor="nw")
+    canvas1.create_text(screen_width/4.5, screen_height/8,
+                        text="Enter Name", font=("Chalkboard SE", 60), fill="#3e2723")
 
-    nameEntry = Entry(nameWindow, width=15, justify='center', font=('Chalkboard SE', 30), bd=5, bg='white')
-    nameEntry.place(x = screen_width/7, y=screen_height/5.5 )
+    nameEntry = Entry(nameWindow, width=15, justify='center',
+                      font=('Chalkboard SE', 30), bd=5, bg='white')
+    nameEntry.place(x=screen_width/7, y=screen_height/5.5)
 
-    button = tk.Button(nameWindow, text="Save", font=("Chalkboard SE", 30),width=11, command=saveName, height=2, bg="red", bd=3)
-    button.place(x = screen_width/6, y=screen_height/4)
+    button = tk.Button(nameWindow, text="Save", font=(
+        "Chalkboard SE", 30), width=11, command=saveName, height=2, bg="red", bd=3)
+    button.place(x=screen_width/6, y=screen_height/4)
 
     nameWindow.resizable(True, True)
     nameWindow.mainloop()
-
-
-
 
 
 def setup():
@@ -205,29 +220,10 @@ def setup():
     global PORT
     global IP_ADDRESS
 
-
     SERVER = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     SERVER.connect((IP_ADDRESS, PORT))
-
-
 
     askPlayerName()
 
 
-
 setup()
-
-
-
-
-
-
-
-
-
-
-
-# NOTE:
-    # Creating Game Window
-    # creating Ticket
-    # Placing Numbers Inside ticket
